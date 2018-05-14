7d6b2c2ed667b3e83f8fc60f9e6e4f5e0486dbbd
Cory Pavitt <corypavitt@hotmail.com>
2018-05-14 15:01:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  bar body = document.body
+  let index = 0
+   body.addEventListener('keydown',function(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+)
